32a52f8a24e68010cb761f840edb9f7f20a3cf6a
Roman Nebesnuy <roman.nebesnuy@gmail.com>
2019-07-15 21:24:40 +0300

    done Anagram

--- exercises/Anagram/Anagram.php
+++ exercises/Anagram/Anagram.php
@@ -4,15 +4,28 @@ declare(strict_types=1);
 
 namespace Exercises\Anagram;
 
+use phpDocumentor\Reflection\Types\Boolean;
+
 /**
  * Two strings are anagrams if same characters are used in both.
  * Only case insensitive characters, not spaces or punctuation should be counted.
  *
- * @method static bool check(string $first, string $second)
  * @example Anagram::check('rail safety', 'fairy tales') === true
  * @example Anagram::check('roast beef', 'goat roast') === false
  * @example Anagram::check('Elvis, 'lives') === true
  */
 final class Anagram
 {
+    public static function check(string $first, string $second)
+    {
+        return self::try1($first, $second);
+    }
+
+    protected static function try1(string $first, string $second)
+    {
+        $first = array_filter(array_map('trim', str_split(strtolower($first))));
+        $second = array_filter(array_map('trim', str_split(strtolower($second))));
+
+        return empty(array_diff($first, $second));
+    }
 }
