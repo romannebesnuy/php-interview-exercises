497fee26003d618a1d301258c30f2d928311563d
Roman Nebesnuy <roman.nebesnuy@gmail.com>
2019-07-16 22:31:28 +0300

    done Capitalize

--- exercises/Capitalize/Capitalize.php
+++ exercises/Capitalize/Capitalize.php
@@ -7,10 +7,23 @@ namespace Exercises\Capitalize;
 /**
  * Capitalize each word.
  *
- * @method static string get(string $string)
  * @example Capitalize::get('hello there') === 'Hello There'
  * @example Capitalize::get("hey, so it's working!") === "Hey, So It's Working!"
  */
 final class Capitalize
 {
+    public static function get(string $string)
+    {
+        return self::try2($string);
+    }
+
+    protected static function try1(string $string)
+    {
+        return ucwords($string);
+    }
+
+    protected static function try2(string $string)
+    {
+        return implode(' ', array_map('ucfirst', explode(' ', $string)));
+    }
 }
