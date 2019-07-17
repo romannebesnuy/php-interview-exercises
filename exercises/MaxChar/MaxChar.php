faf05a84f4da8e4e65539b69245138068883e776
Roman Nebesnuy <roman.nebesnuy@gmail.com>
2019-07-17 10:18:08 +0300

    done MaxChar

--- exercises/MaxChar/MaxChar.php
+++ exercises/MaxChar/MaxChar.php
@@ -7,10 +7,25 @@ namespace Exercises\MaxChar;
 /**
  * Find the most used character.
  *
- * @method static string get(string $string)
  * @example MaxChar::get('qqweqrty')  === 'q'
  * @example MaxChar::get('apple 2202')  === '2'
  */
 final class MaxChar
 {
+    public static function get(string $string)
+    {
+        return self::try2($string);
+    }
+
+    protected static function try1($string)
+    {
+        $array = array_count_values(array_filter(str_split($string)));
+        return (string) array_keys($array, max($array))[0];
+    }
+
+    protected static function try2($string)
+    {
+        $array = array_count_values(array_filter(str_split($string)));
+        return (string) array_search(max($array), $array);
+    }
 }
